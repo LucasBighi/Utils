b92cf36a0a19d8197677912d8a126502757cbe89
Lucas Bighi <lucas.bighi@turbobank.co>
2022-03-08 16:32:39 -0300

    Adiciona conversao de data

--- Sources/Utils/StringExtensions.swift
+++ Sources/Utils/StringExtensions.swift
@@ -12,4 +12,22 @@ public extension String {
     var localized: String {
         return NSLocalizedString(self, comment: "")
     }
+    
+    var isValidCPF: Bool {
+        let cleanText = self.removingManyOcurrences(of: "-",".")
+        return cleanText.count == 11
+    }
+    
+    func removingManyOcurrences(of string: String...) -> String {
+        var cleanString = self
+        string.forEach { cleanString = cleanString.replacingOccurrences(of: $0, with: "") }
+        return cleanString
+    }
+    
+    func toDate(withFormat format: Format) -> Date? {
+        let format = DateFormatter.dateFormat(fromTemplate: format.rawValue, options: 0, locale: .current)!
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        return formatter.date(from: self)
+    }
 }
