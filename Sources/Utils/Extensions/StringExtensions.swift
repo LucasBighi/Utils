de851680f1ba16f26185e5d270884d243e2fa27c
Lucas Bighi <lucas.bighi@turbobank.co>
2022-03-21 16:11:13 -0300

    espaco

--- Sources/Utils/Extensions/StringExtensions.swift
+++ Sources/Utils/Extensions/StringExtensions.swift
@@ -91,4 +91,9 @@ public extension String {
         return false
     }
 
+    func slice(from: String, to: String) -> String? {
+        guard let rangeFrom = range(of: from)?.upperBound else { return nil }
+        guard let rangeTo = self[rangeFrom...].range(of: to)?.lowerBound else { return nil }
+        return String(self[rangeFrom..<rangeTo])
+    }
 }
